60d0f2bf6351f616fb1d0657e0937179b85fc601
Gillys Dantas <gillys.ayres@gmail.com>
2021-05-10 17:00:51 -0300

    solved a problem with order model

--- db/migrate/20210510195729_create_orders.rb
+++ db/migrate/20210510195729_create_orders.rb
@@ -4,14 +4,13 @@ class CreateOrders < ActiveRecord::Migration[6.1]
       t.string :name
       t.string :phone_number
       t.float :total_value
-      t.integer :status
+      t.integer :status, default: 0
       t.references :restaurant, null: false, foreign_key: true
       t.string :city
       t.string :street
       t.string :neighborhood
       t.string :number
       t.string :complement
-      t.references :restaurant, null: false, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,41 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_10_193915) do
+ActiveRecord::Schema.define(version: 2021_05_10_195729) do
+
+  create_table "order_products", force: :cascade do |t|
+    t.integer "quantity"
+    t.integer "order_id", null: false
+    t.integer "product_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["order_id"], name: "index_order_products_on_order_id"
+    t.index ["product_id"], name: "index_order_products_on_product_id"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.string "name"
+    t.string "phone_number"
+    t.float "total_value"
+    t.integer "status", default: 0
+    t.integer "restaurant_id", null: false
+    t.string "city"
+    t.string "street"
+    t.string "neighborhood"
+    t.string "number"
+    t.string "complement"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["restaurant_id"], name: "index_orders_on_restaurant_id"
+  end
+
+  create_table "product_categories", force: :cascade do |t|
+    t.string "title"
+    t.integer "restaurant_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["restaurant_id"], name: "index_product_categories_on_restaurant_id"
+  end
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -27,5 +61,9 @@ ActiveRecord::Schema.define(version: 2021_05_10_193915) do
     t.index ["category_id"], name: "index_restaurants_on_category_id"
   end
 
+  add_foreign_key "order_products", "orders"
+  add_foreign_key "order_products", "products"
+  add_foreign_key "orders", "restaurants"
+  add_foreign_key "product_categories", "restaurants"
   add_foreign_key "restaurants", "categories"
 end

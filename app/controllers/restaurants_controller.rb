2f9329bc846d840ce679cfbfa82312322ebd9adc
Gillys Dantas <gillys.ayres@gmail.com>
2021-05-11 15:36:53 -0300

    fixes a typo

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -27,6 +27,7 @@ class RestaurantsController < ApplicationController
 
   def filter_by_category
     @restaurants = @restaurants.select do |r|
-    r.category.title == params[:category]
- end
+      r.category.title == params[:category]
+    end
+  end
 end
